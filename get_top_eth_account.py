fc039436d5d9c4baef04cff22c1731c65d6eb333
koboyiv <32175605+koboyiv@users.noreply.github.com>
2019-04-14 18:31:16 +0700

    get balance_per_block

--- get_top_eth_account.py
+++ get_top_eth_account.py
@@ -2,21 +2,26 @@ import asyncio
 import json
 import math
 import time
-
+import csv
 from operator import itemgetter
 import aiohttp
 import requests
 import web3
+import numpy as np
 from bs4 import BeautifulSoup
+from sqlalchemy import create_engine, text
 from tqdm import tqdm
+import config
 
-async def http_v1_get(address_list):
+engine = create_engine(config.DB.ethereum(), echo=False)
+
+async def http_v1_get(address_list,block_number):
     msg_list = []
     result = []
 
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect("ws://x.stockradars.co:8546/") as ws:
-            data = '{"jsonrpc": "2.0", "method": "eth_getBalance", "params": ["%s", "0x13880"], "id": %s}'
+            data = json_rpc_block(block_number)
 
             for index, address in enumerate(tqdm(address_list)):
                 await ws.send_str(data % (address, index))
@@ -32,6 +37,12 @@ async def http_v1_get(address_list):
             })
         else:
             print(res)
+
+    engine.execute(text("""
+            INSERT IGNORE INTO ethereum.balance (address, token, blockNumber, balance)
+            VALUES (:address, 'ETH', {}, :balance)
+    """.format(block_number)), result)
+
     return result
 
 
@@ -45,7 +56,7 @@ def get_address():
     result = []
     s = requests.Session()
 
-    for page in tqdm(range(1, 2)):
+    for page in tqdm(range(1,11)):
         r = s.get('https://etherscan.io/accounts/{}?ps=100'.format(page))
         html = r.text
         soup = BeautifulSoup(html, 'html.parser')
@@ -56,26 +67,64 @@ def get_address():
 
     return result
 
+def get_numblocks():
+
+    numblock = []
+    with open('numblocks.csv') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        print(csv_reader)
+
+        for row in csv_reader:
+            numblock.append(row[0])
+        """ Convert str to int list"""
+        numblock = list(map(int, numblock))
+
+    return numblock
+
+def json_rpc_block(numblock):
+    data_font ='{"jsonrpc": "2.0", "method": "eth_getBalance", "params": ["%s", "'
+    hex_block = hex(numblock)
+    data_back = '"], "id": %s}'
+
+    return data_font + hex_block + data_back
 
 if __name__ == '__main__':
+
     all_address = get_address()
-    print(len(all_address))
+    numblock = get_numblocks()
+    numblock = np.asarray(numblock)
 
     start = time.time()
     print(start)
 
     loop = asyncio.get_event_loop()
 
-    return_result = loop.run_until_complete(asyncio.gather(
-        *[http_v1_get(part_address) for part_address in chunks(all_address, math.ceil(len(all_address) / 4.0))]
-    ))
+    result = []
+    for i in numblock:
+        if i == 2:
+            break
+
+        else:
+            return_result = loop.run_until_complete(asyncio.gather(
+                *[http_v1_get(part_address,i) for part_address in chunks(all_address, math.ceil(len(all_address) / 4.0))]
+            ))
+
+            for r in return_result:
+                result += r
+
+            toplist = sorted(result, key=itemgetter('balance'), reverse=True)
+            print(time.time() - start)
+            print("block number:", i)
+            #print(toplist[0:101])
+        result = []
 
     loop.close()
-    print(time.time() - start)
-    result = []
-    for r in return_result:
-        result += r
 
-    toplist = sorted(result, key=itemgetter('balance'), reverse=True)
-    print(toplist[0:101])
+    """  
+        result = []
+        for r in return_result:
+            result += r
 
+        toplist = sorted(result, key=itemgetter('balance'), reverse=True)
+        print(toplist[0:101])
+    """
